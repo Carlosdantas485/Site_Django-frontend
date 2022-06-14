55985cef5500318f94d6cff16711334ff8680492
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-14 17:19:19 -0300

    first modifications

---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function List(){
+    return (
+        <div>
+            <h1>MINHA LISTA</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import List from './List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <List></List>
   </React.StrictMode>
 );
 
