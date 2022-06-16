08c216f9e552399a33bb2cce21986a8355130cbe
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-16 11:49:01 -0300

    first integration with  API

--- src/UserLIsts.js
+++ src/UserLIsts.js
@@ -1,11 +1,32 @@
 import React from "react";
 import List from "./List";
 
-export default function Userlists(){
-    return (
-        <div>
-            <List listName={'minha lista'}/>
-            <List listName={'minha lista2'}/>
-        </div>
-    )
+export default class UserLIsts extends React.Component{
+    
+    state = { lists:null, loading: true }
+
+    async componentDidMount(){
+        const config = {
+            headers: {
+                    'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 769ddbc5b731d3ea185e30256c4e23e5d18240dd';
+
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists:data, loading: false})
+    }
+    render()
+    {
+        return (
+            <div>
+                <List listName={'minha lista'}/>
+                <List listName={'minha lista2'}/>
+            </div>  
+        )
+    }
+   
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Userlists
+import UserLists
  from './UserLIsts';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Userlists/>
+    <UserLists/>
   </React.StrictMode>
 );
 
